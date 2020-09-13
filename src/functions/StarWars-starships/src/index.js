b0b4e84d43826c4a7384901f4d867530a79214c0
cloud303-aloveless <aloveless@cloud303.io>
2020-09-13 13:22:11 -0600

    added lambda logic for star wars api

--- src/functions/StarWars-starships/src/index.js
+++ src/functions/StarWars-starships/src/index.js
@@ -3,8 +3,33 @@ const Axios = require("axios");
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 exports.handler = async (event) => {
-  console.log("event:::", event);
-  return successResponse("Success starships");
+  return getStarWarsStarships();
+};
+
+// gets star wars starships
+const getStarWarsStarships = async () => {
+  try {
+    const response = await Axios.get("https://swapi.dev/api/starships");
+    return putRawS3Data(response.data.results);
+  } catch (error) {
+    return errorResponse(500, error);
+  }
+};
+
+// Puts raw data in s3
+const putRawS3Data = async (data) => {
+  const timestamp = Date.now();
+  const params = {
+    Body: JSON.stringify(data),
+    Bucket: process.env.s3Bucket,
+    Key: `raw/starwars/starships/${timestamp}`,
+  };
+  try {
+    const s3Object = await s3.putObject(params).promise();
+    return successResponse(s3Object);
+  } catch (error) {
+    return errorResponse(500, error);
+  }
 };
 
 // Sends success back to client
@@ -14,9 +39,6 @@ const successResponse = (successMessage) => {
     body: JSON.stringify({
       SuccessMessage: successMessage,
     }),
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
   };
 };
 
@@ -27,8 +49,5 @@ const errorResponse = (code, errorMessage) => {
     body: JSON.stringify({
       ErrorMessage: errorMessage,
     }),
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
   };
 };
